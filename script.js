a8126f340a72ba4b8a924fdd866c2852dadc0eae
Daniel <dkuhrmeyer@gmail.com>
2016-02-10 17:31:25 -0700

    ajax requests working

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+window.pollsterPoll = function(incoming_data){
+    console.log(incoming_data);
+};
+
 $(function() {
     $( "#tabs-1" ).tabs({
-    show: { effect: "blind", duration: 800 }
+    show: { 
+        effect: "blind", 
+        duration: 800 
+    }
+    });
+});
+
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://elections.huffingtonpost.com/pollster/api/polls.json?callback=pollsterPoll',
+        dataType: 'script',
+        cache: true
     });
 });
